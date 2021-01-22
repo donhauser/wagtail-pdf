c1dbcc9fc581f9a854d07d0894717260d752b914
Jonas Donhauser <donhauserjonas@googlemail.com>
2021-01-22 14:53:16 +0100

    added long description

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 import setuptools
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(
     name='wagtail-pdf-view',
-    version='0.1.0',
+    version='0.1.1',
     description='PDF rendering views for the Wagtail CMS',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Jonas Donhauser',
     #author_email='',
     url='https://github.com/donhauser/wagtail-pdf',
