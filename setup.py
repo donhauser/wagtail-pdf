ac2dcbe2a2c19b3ea6e66ea454328bd8187951d0
Jonas Donhauser <donhauserjonas@googlemail.com>
2024-10-08 17:40:47 +0200

    Added recursive directory includes and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 import setuptools
 
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+import os
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+    
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
 
 setuptools.setup(
     name='wagtail-pdf-view',
@@ -15,11 +24,36 @@ setuptools.setup(
     #author_email='',
     url='https://github.com/donhauser/wagtail-pdf',
     packages=['wagtail_pdf_view'],
-    package_data={'': ['LICENSE', 'templates/*', 'static/*']},
+    package_data={
+        '': ['LICENSE']
+        + package_files('wagtail_pdf_view/templates')
+        + package_files('wagtail_pdf_view/static')
+    },
     include_package_data=True,
     python_requires=">=3.8",
     install_requires=["wagtail", "django-weasyprint"],
     extras_require = {
         'django-tex':["django-tex"],
     },
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Framework :: Django",
+        "Framework :: Django :: 4",
+        "Framework :: Django :: 5.0",
+        "Framework :: Django :: 5.1",
+        "Framework :: Wagtail",
+        "Framework :: Wagtail :: 4",
+        "Framework :: Wagtail :: 5",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
 )
